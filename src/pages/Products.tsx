ab911f64a6f29509c1d10382a999049e2466e76a
fisherjacobc <72705185+F1sxher@users.noreply.github.com>
2023-06-19 10:55:42 -0400

    netlify test deploy

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -14,6 +14,7 @@ function Fallback(): JSX.Element {
 type Product = {
     category: string;
     decalID: string;
+    decalUrl: string;
     description: string;
     devproduct_id: string;
     price: string;
@@ -58,9 +59,18 @@ export default function Products(): JSX.Element {
                 
             }).then(res => product.price = res.data.PriceInRobux, (error) => {
                 product.price = "BUY"
-                // console.log(error)
-                // suppress for now^^
+                console.log(error)
             })
+
+            axios(`https://thumbnails.roblox.com/v1/assets?assetIds=${product.decalID}&returnPolicy=PlaceHolder&size=700x700&format=Png&isCircular=false`, {
+                
+            }).then(res => product.decalUrl = res.data.data[0].imageUrl)
+            .catch((error) => {
+                product.decalUrl = "/img/huxleysales.png"
+            })
+
+            console.log(product.decalUrl)
+
             newProducts.push(product)
         })
 
@@ -77,9 +87,13 @@ export default function Products(): JSX.Element {
             <h1 className="text-5xl text-center">Products</h1>
             <div className="px-8 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {data.products.map((product) => (
-                    <a className="flex flex-col justify-center rounded-md drop-shadow-md bg-gradient-to-t from-cyan-500 dark:to-[#1b1b1d] min-h-[8rem]" href={`https://www.roblox.com/games/start?placeId=11746877837&launchData=${product.productID}`} target="_blank" style={{"textDecoration": "none"}}>
-                        <span className="w-[100%] p-2 text-3xl font-black dark:text-gray-50 text-gray-950 text-center">{product.name}</span>
-                        <span className="mb-2 mx-8 md:mx-24 lg:mx-28 xl:mx-24 2xl:mx-40 bg-gray-50 dark:bg-gray-950 rounded-md ring-2 ring-green-500 text-2xl font-semibold text-green-500 text-center"><span className="font-bold">R$ </span>{product.price || "BUY"}</span>
+                    <a className="flex flex-col justify-center rounded-md drop-shadow-md bg-gradient-to-t from-cyan-500 dark:to- min-h-[8rem]" href={`https://www.roblox.com/games/start?placeId=11746877837&launchData=${product.productID}`} target="_blank" style={{"textDecoration": "none"}}>
+                        <img src={product.decalUrl}/>
+                        <span>{product.decalUrl}</span>
+                        <div className="z-10 flex flex-col justify-center">
+                            <span className="w-[100%] p-2 text-3xl font-black dark:text-gray-50 text-gray-950 text-center">{product.name}</span>
+                            <span className="mb-2 mx-8 md:mx-24 lg:mx-28 xl:mx-24 2xl:mx-40 bg-gray-50 dark:bg-gray-950 rounded-md ring-2 ring-green-500 text-2xl font-semibold text-green-500 text-center"><span className="font-bold">R$ </span>{product.price || "BUY"}</span>
+                        </div>
                     </a>
                 ))} 
             </div>
