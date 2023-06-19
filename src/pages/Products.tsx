73c2d42a77383dd92c130ed3a3665313528f9714
fisherjacobc <72705185+F1sxher@users.noreply.github.com>
2023-06-19 15:18:40 -0400

    products W

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -35,7 +35,6 @@ type HubInfo = {
 export default function Products(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
 
-  const [firstData, setFirstData] = useState<boolean>(false);
   const [loaded, setLoaded] = useState<boolean>(false);
   const [data, setData] = useState<HubInfo>(null);
 
@@ -44,55 +43,46 @@ export default function Products(): JSX.Element {
         headers: {
             "hub-secret-key": siteConfig.customFields.hubSecret as string
         }
-    }).then(res => {
-        setData(res.data.details)
-        setFirstData(true)
-    })
-  }, [])
-
-  useEffect(() => {
-    if(!firstData) return
+    }).then(async res => {
         const newProducts: Product[] = []
-        
-        data.products.forEach((product) => {
-            axios(`https://economy.roblox.com/v2/developer-products/${product.devproduct_id}/details`, {
-                
-            }).then(res => product.price = res.data.PriceInRobux, (error) => {
-                product.price = "BUY"
-                console.log(error)
-            })
 
-            axios(`https://thumbnails.roblox.com/v1/assets?assetIds=${product.decalID}&returnPolicy=PlaceHolder&size=700x700&format=Png&isCircular=false`, {
-                
-            }).then(res => product.decalUrl = res.data.data[0].imageUrl)
-            .catch((error) => {
+        res.data.details.products.forEach(async (product: Product, index: number) => {
+            const gottenPrice = await axios(`https://hux.fisherdev.workers.dev/price?id=${product.devproduct_id}`)
+            product.price = gottenPrice.data.price
+
+            if (product.decalID != "9690630240") {
+                const gottenImage = await axios(`https://hux.fisherdev.workers.dev/image?id=${product.decalID}`)
+                product.decalUrl = gottenImage.data.image
+            } else {
                 product.decalUrl = "/img/huxleysales.png"
-            })
-
-            console.log(product.decalUrl)
+            }
 
             newProducts.push(product)
+
+            if (res.data.details.products.length === newProducts.length) {
+                setData({
+                    hubId: res.data.details.hubId,
+                    products: newProducts
+                })
+                setLoaded(true)
+            }
         })
 
-        setData({
-            hubId: data.hubId,
-            products: newProducts
-        })
-        setLoaded(true)
-  }, [firstData])
+        console.log(newProducts)
+    })
+  }, [])
 
   return (
     <Layout>
         {loaded && <div>
             <h1 className="text-5xl text-center">Products</h1>
-            <div className="px-8 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="px-8 py-3 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {data.products.map((product) => (
-                    <a className="flex flex-col justify-center rounded-md drop-shadow-md bg-gradient-to-t from-cyan-500 dark:to- min-h-[8rem]" href={`https://www.roblox.com/games/start?placeId=11746877837&launchData=${product.productID}`} target="_blank" style={{"textDecoration": "none"}}>
-                        <img src={product.decalUrl}/>
-                        <span>{product.decalUrl}</span>
-                        <div className="z-10 flex flex-col justify-center">
+                    <a className="flex flex-col justify-center aspect-video drop-shadow-md min-h-[8rem]" href={`https://www.roblox.com/games/start?placeId=11746877837&launchData=${product.productID}`} target="_blank" style={{"textDecoration": "none"}}>
+                        <img src={product.decalUrl} className="h-full absolute overflow-clip left-0 right-0 mx-auto rounded-md"/>
+                        <div className="z-10 flex flex-col rounded-md justify-end bg-gradient-to-t from-cyan-500 to-transparent w-full h-full">
                             <span className="w-[100%] p-2 text-3xl font-black dark:text-gray-50 text-gray-950 text-center">{product.name}</span>
-                            <span className="mb-2 mx-8 md:mx-24 lg:mx-28 xl:mx-24 2xl:mx-40 bg-gray-50 dark:bg-gray-950 rounded-md ring-2 ring-green-500 text-2xl font-semibold text-green-500 text-center"><span className="font-bold">R$ </span>{product.price || "BUY"}</span>
+                            <span className="mb-2 mx-8 md:mx-24 lg:mx-28 xl:mx-24 2xl:mx-40 bg-gray-50 dark:bg-gray-950 rounded-md ring-2 ring-green-500 text-2xl font-semibold text-green-500 text-center"><span className="font-bold">R$ </span>{product.price}</span>
                         </div>
                     </a>
                 ))} 
